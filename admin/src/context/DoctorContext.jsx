069ebbcf3de10dabfc8c54a2fb27e23c8693a2c7
Vivek kumar singh <167025814+vivek-ksinghg@users.noreply.github.com>
2025-12-21 21:36:35 +0530

    Update DoctorContext.jsx

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -6,7 +6,7 @@ export const DoctorContext=createContext();
 
 const DoctorContextProvider=(props)=>{
 
-    const backendUrl=import.meta.env.VITE_BACKEND_URL
+    const backendUrl="https://prescripto-backend-bpvo.onrender.com"
     const [dToken,setDToken]=useState(localStorage.getItem('dToken')?localStorage.getItem('dToken'): '')
     const[appointments,setAppointments]=useState([])
     const[dashData,setDashData]=useState(false)
